ac29202b179a98b62d8eb781b38f81529b5d9de1
Brandon Cervantes <42504242+Slevkro@users.noreply.github.com>
2020-11-06 15:31:16 -0600

    Practicas 1-4 completas
    Practicas de laboratorio de bases de datos, en su mayoría de modelado

---
+++ Practica-01/s-01-resultados-enc.sh
@@ -0,0 +1,67 @@
+RP^3:?^32D9
+649@ Q'2=:525@C !Cá4E:42 ` 4@>A=6>6?E2C:2Q
+D@FC46 ]^D\__\7F?4:@?6D\G2=:524:@?]D9
+
+649@ QQ
+7G096256C
+
+649@ QQ
+
+RG2=:52 6= FDF2C:@ 56 6;64F4:ó?
+, QSL&$t#NQ Pl Q@C24=6Q \2 QSL&$t#NQ Pl QC@@EQ .j 4@?5:4:@?lSn
+
+R !2C2> `i }ú>6C@ 56 C6?8=ó?
+R !2C2> ai $E2EFD \ C6DF=E25@ 56 =2 G2=:524:ó?
+R !2C2> bi '2=@C 6DA6C25@
+R !2C2> ci '2=@C @3E6?:5@
+R !2C2> di |6?D2;6 A@D:E:G@
+R !2C2> ei |6?D2;6 56 6CC@C
+7G02DD6CE ` S4@?5:4:@? QFDF2C:@ Pl @C24=6[ C@@EQ QSL&$t#NQ -
+  Q&DF2C:@ 56 6;64F4:ó? 4@CC64E@i SL&$t#NQ Q&DF2C:@ 56 6;64F4:ó? :?Gá=:5@Q
+
+R'2=:52 ~#pr{t0w~|t
+, \5 S~#pr{t0w~|t .j 4@?5:4:@?lSn
+7G02DD6CE a S4@?5:4:@? Qs:C64E@C:@ ?@ 6I:DE6Q QSL~#pr{t0w~|tNQ -
+  Q~#pr{t0w~|ti SL~#pr{t0w~|tNQ Qs:C64E@C:@ ~#pr{t0w~|t ?@ 6I:DE6Q
+
+R G2=:52 DJD4E= \B 7D]2:@\>2I\?C
+A2C2>0<6C?6=l1DJD4E= \B 7D]2:@\>2I\?C1
+, QSA2C2>0<6C?6=Q l Q7D]2:@\>2I\?C l `_cgdfeQ .j 4@?5:4:@?lSn
+7G02DD6CE b S4@?5:4:@? Q7D]2:@\>2I\?C l `_cgdfeQ QSA2C2>0<6C?6=Q -
+  Q!2Cá>6EC@ 7D]2:@\>2I\?C 4@CC64E@Q Q!2Cá>6EC@ 7D]2:@\>2I\?C :?4@CC64E@Q
+
+R'2=:52 E2>2ñ@ 56= FDF2C:@ @C24=6
+7:=60D:K6l1DE2E \4TD S~#pr{t0w~|t^3:?^@C24=61
+P , QS7:=60D:K6Q l Q_Q .j 4@?5:4:@?lSn
+7G02DD6CE c S4@?5:4:@? Q%2>2ñ@ >2J@C 2 46C@Q[ Q_Q -
+  Q%2>2ñ@ 4@CC64E@ A2C2 6= 2C49:G@ @C24=6 SL7:=60D:K6NQ -
+  Q%2>2ñ@ :?4@CC64E@ A2C2 6= 2C49:G@ @C24=6 SL7:=60D:K6NQ
+
+R!6C>:D@D 6DA64:2=6D 56= 2C49:G@ @C24=6
+A6C>:D@Dl1DE2E \4 VTpV S~#pr{t0w~|t^3:?^@C24=61
+, QSLA6C>:D@DNQ l Q\CHDC\D\\IQ .j 4@?5:4:@?lSn
+7G02DD6CE d S4@?5:4:@? Q\CHDC\D\\IQ QSLA6C>:D@DNQ -
+  Q!6C>:D@D 4@CC64E@D A2C2 6= 2C49:G@ @C24=6 SLA6C>:D@DNQ -
+  Q!6C>:D@D :?4@CC64E@D A2C2 6= 2C49:G@ @C24=6 SLA6C>:D@DNQ
+
+RG2=:52 6= E2>2ñ@ 56= 2C49:G@ =D?C4E= 
+7:=60D:K6l1DE2E \4TD S~#pr{t0w~|t^3:?^=D?C4E=1
+P , QS7:=60D:K6Q l Q_Q .j 4@?5:4:@?lSn
+7G02DD6CE e S4@?5:4:@? Q%2>2ñ@ >2J@C 2 46C@Q[ Q_Q -
+  Q%2>2ñ@ 4@CC64E@ A2C2 6= 2C49:G@ =D?C4E= SL7:=60D:K6NQ -
+  Q%2>2ñ@ :?4@CC64E@ A2C2 6= 2C49:G@ =D?C4E= SL7:=60D:K6NQ
+  
+RG2=:52 6= E2>2ñ@ 56= 2C49:G@ DB=A=FD 
+7:=60D:K6l1DE2E \4TD S~#pr{t0w~|t^3:?^DB=A=FD1
+P , QS7:=60D:K6Q l Q_Q .j 4@?5:4:@?lSn
+7G02DD6CE f S4@?5:4:@? Q%2>2ñ@ >2J@C 2 46C@Q[ Q_Q -
+  Q%2>2ñ@ 4@CC64E@ A2C2 6= 2C49:G@ DB=A=FD SL7:=60D:K6NQ -
+  Q%2>2ñ@ :?4@CC64E@ A2C2 6= 2C49:G@ DB=A=FD SL7:=60D:K6NQ
+
+649@ QQ
+649@ Qllllllll a] |@DEC2?5@ C49:G@D 56 :?DE2=24:@? lllllllllllllQ
+=D \= S~#pr{t0w~|t^3:?^@C24=6
+=D \= S~#pr{t0w~|t^3:?^DB=A=FD
+=D \= S~#pr{t0w~|t^3:?^=D?C4E= 
+=D \= O^]32D9C4
+649@ Q{:DE@ PQ
